d190ccccf20118e27bf45e4972fe9231d41492d8
qorlgns1 <rlgns0610@gmail.com>
2021-08-30 00:49:22 +0900

    styled-components 매뉴얼에서 제공하는 유틸 함수

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -1,11 +1,30 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+//위에 있는 SIZE 객체에 따라 자동으로 MEDIA 쿼리 함수를 만들어 줍니다.
+//참고 : https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props 로 넣어준 값을 직접 전달해줄 수 있습니다. */
   background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`}
 `;
 
 const Button = styled.button`
